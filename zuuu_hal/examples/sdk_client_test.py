cbdfdd84668a1ffb3fe65a2e3d924ae8d43c83fb
RemiFabre <remifabre1800@gmail.com>
2025-01-29 16:13:16 +0100

    minor

--- zuuu_hal/examples/sdk_client_test.py
+++ zuuu_hal/examples/sdk_client_test.py
@@ -24,7 +24,7 @@ print("Testing the timeout, the robot should not reach x=10.0")
 reachy.mobile_base.goto(x=10.0, y=0.0, theta=00.0, wait=False, degrees=True, distance_tolerance=0.05, angle_tolerance=5.0, timeout=2)
 reachy.mobile_base.goto(x=0.0, y=0.0, theta=00.0, wait=False, degrees=True, distance_tolerance=0.05, angle_tolerance=5.0, timeout=10000)
 
-print("Note: the timeout had an effect because there was another goto scheduled, otherwise the mobile base would continue to go to x=10.0. TODO check if this is what we want")
+print("Note: the timeout had an effect because there was another goto scheduled, otherwise the mobile base would continue to go to x=10.0. -> Validated with Gaëlle.")
 
 # TODO Remi x Team interface, wait=True fails
 
