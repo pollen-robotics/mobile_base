3c68527454d805b6db7288b1d61816f5bacf3a11
glannuzel <gaelle.lannuzel@gmail.com>
2025-02-06 16:01:33 +0100

    black + isort

--- zuuu_description/launch/description_bringup.launch.py
+++ zuuu_description/launch/description_bringup.launch.py
@@ -4,8 +4,7 @@ import launch
 import launch_ros
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import (Command, FindExecutable, LaunchConfiguration,
-                                  PathJoinSubstitution)
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
